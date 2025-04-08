3ddaeb93727c6a1f1f246fcb1b41d454ff3d3600
syab-syab <syabsyabsyabsyab@gmail.com>
2025-04-08 17:01:30 +0900

    とりあえずプレースホルダー的なものは書いた

---
+++ contents/fontChange.tsx
@@ -0,0 +1,17 @@
+// 出来ればバックグラウンドとかを使いたい
+// コンテンツスクリプトは何となく使いたくない
+
+// 読みづらいサイト
+// https://fromkato.com/webdev/css/properties/font-family
+
+// フォントファミリー
+// https://developer.mozilla.org/ja/docs/Web/CSS/font-family
+// https://fromkato.com/webdev/css/properties/font-family
+
+const fontChange = () => {
+  return (
+    <></>
+  )
+}
+
+export default fontChange
--- popup.tsx
+++ popup.tsx
@@ -3,22 +3,59 @@ import { useState } from "react"
 function IndexPopup() {
   const [data, setData] = useState("")
 
+
+  // https://developer.mozilla.org/ja/docs/Web/CSS/font
+  // font: XX;で一括指定できるっぽい
+
   return (
     <div
       style={{
-        padding: 16
+        width: "200px"
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+        <h2>
+          文字の大きさ
+        </h2>
+        {/* pxで指定 */}
+        {/* font-size */}
+        <h2>
+          文字の太さ
+        </h2>
+        {/* boldとかnormalで指定 */}
+        {/* font-weight */}
+        <h2>
+          文字の色
+        </h2>
+        <input type="color" name="" id="" />
+        {/* inputで指定 */}
+        {/* color */}
+        <h2>
+          文字の間隔
+        </h2>
+        {/* letter-spacingを使う */}
+        {/* pxで指定 */}
+        <h2>
+          文字の横幅
+        </h2>
+        {/* font-stretchで指定できるっぽいけど */}
+        {/* 必要無いかも */}
+        <h2>
+          文字の傾き
+        </h2>
+        {/* font-styleで斜体とかしいて言できるっぽい */}
+        {/* 必要無いかも */}
+        <h2>
+          下線
+        </h2>
+        {/* 有りか無しかだけ聞く */}
+        {/* border */}
+        <h2>
+          フォントの種類
+        </h2>
+        {/* セレクトボックスで聞く */}
+        <h2>
+          背景色
+        </h2>
+        {/* 必要無いかも */}
     </div>
   )
 }
