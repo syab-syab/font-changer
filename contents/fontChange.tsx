709abd0396509f422359be95eeb5307980b58740
syab-syab <syabsyabsyabsyab@gmail.com>
2025-04-10 14:39:52 +0900

    一時保存

--- contents/fontChange.tsx
+++ contents/fontChange.tsx
@@ -3,14 +3,38 @@
 
 // 読みづらいサイト
 // https://fromkato.com/webdev/css/properties/font-family
+// https://creative.eccom.jp/2020/
 
 // フォントファミリー
 // https://developer.mozilla.org/ja/docs/Web/CSS/font-family
 // https://fromkato.com/webdev/css/properties/font-family
+import type { PlasmoCSConfig } from "plasmo"
+import { useState } from "react"
+import { useStorage } from "@plasmohq/storage/hook"
+import { localKey } from "popup"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"],
+}
+
+type Props = {
+  changeVal: boolean,
+  weight: string,
+  color: string,
+  family: string
+}
 
 const fontChange = () => {
+
+
+
+
+
   return (
-    <></>
+    // <></>
+    <div>
+      {localKey.weight}
+    </div>
   )
 }
 
--- popup.tsx
+++ popup.tsx
@@ -1,61 +1,105 @@
 import { useState } from "react"
+import { useStorage } from "@plasmohq/storage/hook"
+
+export const localKey = {
+  weight: "local-font-weight"
+}
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [weight, setWeight] = useState("normal")
+  const [color, setColor] = useState("#000000")
+  const [family, setFamily] = useState("serif")
 
+  const setResetState = () => {
+    setWeight("normal")
+    setColor("#000000")
+    setFamily("serif")
+  }
 
   // https://developer.mozilla.org/ja/docs/Web/CSS/font
   // font: XX;で一括指定できるっぽい
+
 
   return (
     <div
       style={{
         width: "200px"
       }}>
-        <h2>
-          文字の大きさ
-        </h2>
-        {/* pxで指定 */}
-        {/* font-size */}
-        <h2>
-          文字の太さ
-        </h2>
-        {/* boldとかnormalで指定 */}
-        {/* font-weight */}
-        <h2>
-          文字の色
-        </h2>
-        <input type="color" name="" id="" />
-        {/* inputで指定 */}
-        {/* color */}
-        <h2>
-          文字の間隔
-        </h2>
-        {/* letter-spacingを使う */}
-        {/* pxで指定 */}
-        <h2>
-          文字の横幅
-        </h2>
-        {/* font-stretchで指定できるっぽいけど */}
-        {/* 必要無いかも */}
-        <h2>
-          文字の傾き
-        </h2>
-        {/* font-styleで斜体とかしいて言できるっぽい */}
-        {/* 必要無いかも */}
-        <h2>
-          下線
-        </h2>
-        {/* 有りか無しかだけ聞く */}
-        {/* border */}
-        <h2>
-          フォントの種類
-        </h2>
-        {/* セレクトボックスで聞く */}
-        <h2>
-          背景色
-        </h2>
-        {/* 必要無いかも */}
+
+        <div>
+          {/* <label>
+            文字の大きさ
+          </label> */}
+          {/* pxより%で指定した方が良い？ */}
+          {/* XX倍とかで指定したい */}
+          {/* font-size */}
+        </div>
+
+        <div>
+          <label>
+            文字の太さ
+          </label>
+          {/* font-weight */}     
+          <select name="" id="" value={weight} onChange={(e) => setWeight(e.target.value)}>
+            <option value="normal">普通</option>
+            <option value="bold">濃く</option>
+            <option value="lighter">薄く</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="color">
+            文字の色
+          </label>
+          <input type="color" id="color" value={color} onChange={(e) => setColor(e.target.value)} />     
+        </div>
+
+        <div>
+          {/* <label>
+            文字の間隔
+          </label> */}
+          {/* letter-spacing */}
+        </div>
+
+        <div>
+          {/* <label>
+            文字の傾き
+          </label> */}
+          {/* font-style */}       
+        </div>
+
+        <div>
+          {/* <label>
+            行間
+          </label> */}
+          {/* line-heightで設定できる */}          
+        </div>
+
+        <div>
+          <label>
+            フォントの種類
+          </label>
+          <select value={family} onChange={(e) => setFamily(e.target.value)}>
+            <option value="serif">serif</option>
+            <option value="sans-serif">sans-serif</option>
+            <option value="cursive">cursive</option>
+            <option value="fantasy">fantasy</option>
+            <option value="monospace">monospace</option>
+          </select>      
+        </div>
+
+        <div>
+          {/* <label>
+            背景色
+          </label> */}
+          {/* 必要無いかも */}          
+        </div>
+
+        <div>
+          <button onClick={() => setResetState() }>
+            適用
+          </button>
+        </div>
     </div>
   )
 }
